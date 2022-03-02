bc6ffa9ce1e552933afe120db58786a6c79e0a0a
Prasad <dhebprasadshivaji@gmail.com>
2022-03-02 17:44:42 +0530

    header componet complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import {Switch,Route} from 'react-router-dom';
+import React from 'react';
+import HomePage from '../src/pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+// const HatsPage=()=>(
+//   <div>
+//     <h1> HATS PAGE </h1>
+//   </div>
+// )
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+<Header/>
+<Switch>
+    <Route exact path='/' component={HomePage}/>
+     <Route path='/shop' component={ShopPage}/> 
+</Switch>
+
     </div>
   );
 }
